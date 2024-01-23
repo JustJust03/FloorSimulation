ad3227fc3453c708f6664747a6fceadad13528d3
JustJustP <justhogenelst3@gmail.com>
2024-01-23 17:42:30 +0100

    Ready for new full system.

--- StaticComponents/Floor.cs
+++ StaticComponents/Floor.cs
@@ -70,11 +70,11 @@ namespace FloorSimulation
             Display = di;
 
             //layout = new SLayoutDayId(this, rd);
-            //layout = new SLayoutDayIdBuffhub(this, rd);
+            layout = new SLayoutDayIdBuffhub(this, rd);
             //layout = new SLayoutDayIdBuffhub2Streets(this, rd);
             //layout = new SLayoutDayId2Streets(this, rd);
             //layout = new KortereVerdeelstraatSlayout(this, rd);
-            layout = new KortereVerdeelstraatSlayoutSmartStart(this, rd);
+            //layout = new KortereVerdeelstraatSlayoutSmartStart(this, rd);
 
             //layout = new LowPadSlayoutBuffhub(this, rd);
 
